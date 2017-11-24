701a4659887cf2de074cf0c5fca614036991bd96
vbalasu <vbalasu@gmail.com>
2017-11-24 08:01:20 -0500

    Add files via upload

---
+++ callAzureFunction.php
@@ -0,0 +1,33 @@
+<?php
+echo callAzureFunction("https://cloudmaticafunctions.azurewebsites.net/api/echo?code=JPPXiWIHCmGciaQ6Kj4qpmayDkXcIBFBi68GXziYkMMeWAUMIZ388Q%3D%3D&clientId=default", "{\"text\":\"PHP Postman\"}");
+
+function callAzureFunction($url, $json) {
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	  CURLOPT_URL =>
+	$url,
+	  CURLOPT_RETURNTRANSFER => true,
+	  CURLOPT_ENCODING => "",
+	  CURLOPT_MAXREDIRS => 10,
+	  CURLOPT_TIMEOUT => 30,
+	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	  CURLOPT_CUSTOMREQUEST => "POST",
+	  CURLOPT_POSTFIELDS => $json,
+	  CURLOPT_HTTPHEADER => array(
+		"cache-control: no-cache",
+		"content-type: application/json"
+	  ),
+	));
+
+	$response = curl_exec($curl);
+	$err = curl_error($curl);
+
+	curl_close($curl);
+
+	if ($err) {
+	  echo "cURL Error #:" . $err;
+	} else {
+	  return $response;
+	}
+}
